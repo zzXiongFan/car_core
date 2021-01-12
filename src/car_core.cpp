b0cfacc9ee4a5d5282ecd7aec222f8718a7dd3f2
zhangjunwang <zhangjunwangmail@qq.com>
2021-01-12 18:14:38 +0800

    fix

--- src/car_core.cpp
+++ src/car_core.cpp
@@ -1,5 +1,15 @@
 #include <car_core.h>
 
+// odom 信息
+typedef struct {
+  float x;
+  float y;
+  Eigen::Quaterniond quater;
+}Odom;
+
+Odom last_odom_;
+bool odom_init = false;
+
 // 新建线程回调
 void pgvCallbackThread() {
   ROS_INFO_STREAM("Callback thread id=" << boost::this_thread::get_id());
@@ -46,12 +56,31 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
   Eigen::Quaterniond cur_quaternion4(cur_orientation.w, cur_orientation.x, cur_orientation.y, cur_orientation.z);
   // 计算两者差值
   double cur_z = cur_quaternion4.toRotationMatrix().eulerAngles(2, 1, 0)[0];
+  if(odom_init == false) {
+    last_odom_.x = msg->pose.pose.position.x;
+    last_odom_.y = msg->pose.pose.position.y;
+    last_odom_.quater = cur_quaternion4;
+    odom_init = true;
+  }
+  double last_z = last_odom_.quater.toRotationMatrix().eulerAngles(2, 1, 0)[0];
+  // TODO: 确定此处的正反: 使用更优雅的方式
+  double diff_angle = cur_z - last_z;
+  // 此处计算时: 若二者差距过大：代表越过 PI 界
+  if(diff_angle < -2) {
+    diff_angle = (cur_z + PI) - last_z;
+  }else if(diff_angle > 2) {
+    diff_angle = cur_z - (last_z + PI);
+  }
 
   GlobalPosition pos = {
     .x = msg->pose.pose.position.x,
     .y = msg->pose.pose.position.y,
-    .angle = cur_z,
+    .angle = (loc.getPosition().angle + diff_angle),
+    // .angle = cur_z
   };
+  if(pos.angle > 2*PI) pos.angle = pos.angle - 2*PI;
+  if(pos.angle < 0) pos.angle = pos.angle + 2*PI;
+  last_odom_.quater = cur_quaternion4;
   loc.setPositon(pos);
   // std::cout<< "[ODOM] recived: [ \t" << pos.x << ",\t"<< pos.y << ",\t"<< pos.angle << "\t ]" <<std::endl;
 }
@@ -75,10 +104,10 @@ int main(int argc, char **argv)
   boost::thread PGV_thread(pgvCallbackThread); 
 
   // Odom 订阅处理线程
-  // ros::SubscribeOptions opt_odom = ros::SubscribeOptions::create<nav_msgs::Odometry>
-  //   ("/odom", 10, odomCallback, ros::VoidPtr(), &odom_queue);
-  // ros::Subscriber sub_odom = n.subscribe(opt_odom);
-  // boost::thread ODOM_thread(odomCallbackThread); 
+  ros::SubscribeOptions opt_odom = ros::SubscribeOptions::create<nav_msgs::Odometry>
+    ("/odom", 10, odomCallback, ros::VoidPtr(), &odom_queue);
+  ros::Subscriber sub_odom = n.subscribe(opt_odom);
+  boost::thread ODOM_thread(odomCallbackThread); 
 
   // 主进程添加消息发布逻辑
   ros::Publisher pub = n.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
@@ -90,13 +119,14 @@ int main(int argc, char **argv)
   ros::Rate r(20);
   while (n.ok())
   {
+    // controller.getTwist();
     pub.publish(controller.getTwist());
     r.sleep();
   }
  
   // 等待对应的线程结束
   PGV_thread.join();
-  // ODOM_thread.join();
+  ODOM_thread.join();
   return 0;
 }
 
